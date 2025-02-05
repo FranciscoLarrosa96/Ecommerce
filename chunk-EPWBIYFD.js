86d090b71b69739e0c9571dc1fe67263d0e280b7
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-02-05 17:49:17 -0300

    Auto-generated commit

---
+++ chunk-EPWBIYFD.js
@@ -0,0 +1,1 @@
+var e={production:!0,API_AUTH:"https://ecommerce2024-backend.onrender.com/api",API_USER:"https://ecommerce2024-backend.onrender.com/api/user",API_MP:"https://ecommerce2024-backend.onrender.com/api/mercadopago"};export{e as a};
