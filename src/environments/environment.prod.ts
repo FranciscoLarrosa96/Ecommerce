34dc69cba4f5c269a439d7c341f5cbb1e33779b9
Francisco Larrosa <franciscolarrosa22@gmail.com>
2025-02-05 17:49:06 -0300

    Feat : se agrego url mercado pago

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: true,
     API_AUTH: 'https://ecommerce2024-backend.onrender.com/api',
     API_USER: 'https://ecommerce2024-backend.onrender.com/api/user',
+    API_MP: 'https://ecommerce2024-backend.onrender.com/api/mercadopago',
 };
 // Para generar el build usar este comando "ng build --configuration=prod" esto genera el dist para usar en la carpeta static del backendque se despliegue en produccion
 // Para generar el build para github pages usar este comando "ng build --configuration=github-pages" y luego npx angular-cli-ghpages --dir=dist/ecommerce/browser para subir a github pages
